93bff50151dd155d72a4e37391c921f98aa2abf9
tknakamura <nakamura@likepass.net>
2025-10-10 17:34:52 +0900

    Add Google Tag Manager to Streamlit dashboard and Flask template

--- csv_to_html_dashboard.py
+++ csv_to_html_dashboard.py
@@ -625,6 +625,27 @@ def main():
         layout="wide"
     )
     
+    # Google Tag Manager
+    gtm_head = """
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KLXFVW7G');</script>
+    <!-- End Google Tag Manager -->
+    """
+    
+    gtm_body = """
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KLXFVW7G"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    """
+    
+    st.markdown(gtm_head, unsafe_allow_html=True)
+    st.markdown(gtm_body, unsafe_allow_html=True)
+    
     st.title("MOODMARK｜CSV to HTML コンバーター")
     st.markdown('<p style="font-size: 14px; color: #666; text-align: center; margin-top: -10px;">developed by Takeshi Nakamura</p>', unsafe_allow_html=True)
     st.markdown("---")
